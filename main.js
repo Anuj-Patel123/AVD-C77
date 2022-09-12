d6cfb8371ac91fe86d5677f831a5c257b74d489a
Anuj-Patel123 <106400113+Anuj-Patel123@users.noreply.github.com>
2022-09-11 19:22:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var first_name = "Anuj";
+var last_name = "Patel";
+var full_name = "";
+
+var a = 15;
+var b = 5;
+var add= a + b ;
+var multiply= a * b ;
+var subtraction= a - b ;
+var division= a / b ;
+
+console.log("This is My first console project which is made by...");
+
+console.log(first_name);
+console.log(last_name);
+
+full_name = first_name.concat(last_name);
+console.log(full_name);
+
+console.log(a);
+console.log(b);
+console.log("Add");
+console.log(add);
+console.log("Multiply");
+console.log(multiply);
+console.log("Subtract");
+console.log(subtraction);
+console.log("Divide");
+console.log(division);
